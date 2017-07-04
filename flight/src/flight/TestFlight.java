cc2ef42d1a125b2ec0141d86c83d1ff12d2330c0
rajes <rajes@10.0.0.117>
2017-07-04 18:43:10 -0400

    junit test included

---
+++ flight/src/flight/TestFlight.java
@@ -0,0 +1,17 @@
+package flight;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestFlight {
+
+	@Test
+	public void test() {
+		flight te = new flight();
+		int result=te.addnum(10, 20);
+		assertTrue(result==30);
+		
+	}
+
+}
