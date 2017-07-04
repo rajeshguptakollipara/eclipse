16de582ec4f8f6240a5612c86d79bd28f0d05497
rajesh <rajesh.kollipara93@gmail.com>
2017-07-04 17:07:08 -0400

    this is the first commit

---
+++ flight/src/flight/calculator.java
@@ -0,0 +1,12 @@
+package flight;
+
+public class calculator {
+	public static void main (String[] args)
+	{
+		flight fli =new flight();
+		int k=fli.addnum(10, 20);
+		System.out.println(k);
+		//this is the first committt
+	}
+
+}
---
+++ flight/src/flight/flight.java
@@ -0,0 +1,24 @@
+package flight;
+
+public class flight {
+	
+	public int addnum(int a, int b)
+	{
+		return (a + b);
+	}
+
+	public int subnum(int a, int b)
+	{
+		return (a - b);
+	}
+	public int mulnum(int a, int b)
+	{
+		return (a * b);
+	}
+	public int divnum(int a, int b)
+	{
+		return (a / b);
+	}
+	
+	
+}
